c5a58cac1e20bae03fcf8ef37d478c783bfe8cec
Piotr Lac <piotr.lac@protonmail.ch>
2017-12-02 11:29:45 +0100

    add article view file

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new', as: 'new_article'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
