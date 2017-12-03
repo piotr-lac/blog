a85335976dae9104bee0bff087ce160d519f4010
Piotr Lac <piotr.lac@protonmail.ch>
2017-12-03 16:16:12 +0100

    add errors to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,11 @@ class CommentsController < ApplicationController
     comment_params = params.require(:comment).permit(:commenter, :body)
     @comment = Comment.new(article_id: @article.id)
     @comment.attributes = comment_params
-    @comment.save
-    redirect_to article_path(@comment.article_id)
+    if @comment.save
+      redirect_to article_path(@comment.article_id)
+    else 
+      render 'articles/show'
+    end
   end
 
   def destroy
