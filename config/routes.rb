386545c65855ca2c819d7b34b8773ff8c4326a57
Piotr Lac <piotr.lac@protonmail.ch>
2017-12-03 11:59:54 +0100

    new controller comments

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
   # delete 'articles/:id', to:'articles#destroy'
 
   resources :articles
+  resources :comments
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
