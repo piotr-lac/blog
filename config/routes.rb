18ab75e602ba645989648944475e2d50116d64aa
Piotr Lac <piotr.lac@protonmail.ch>
2017-12-03 10:33:21 +0100

    delete records

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   get 'articles/:id/edit', to:'articles#edit', as: 'edit_article'
   patch 'articles/:id', to:'articles#update'
 
+  delete 'articles/:id', to:'articles#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
