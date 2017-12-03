bf60016baee75d30b88ed0511140561c6b8088c8
Piotr Lac <piotr.lac@protonmail.ch>
2017-12-03 15:29:43 +0100

    validation of the comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,19 @@
 class CommentsController < ApplicationController
-  def create 
-  	comment_params = params.require(:comment).permit(:commenter, :body, :article_id)
-  	@comment = Comment.new(comment_params)
-  	@comment.save
-  	redirect_to article_path(@comment.article_id)
+ 
+ def create
+    @article = Article.find(params[:article_id])
+    comment_params = params.require(:comment).permit(:commenter, :body)
+    @comment = Comment.new(article_id: @article.id)
+    @comment.attributes = comment_params
+    @comment.save
+    redirect_to article_path(@comment.article_id)
   end
+
+  def destroy
+  	@comment = Comment.find(params[:id])
+  	@comment.destroy
+    redirect_to article_path(@comment.article)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,9 @@ Rails.application.routes.draw do
 
   # delete 'articles/:id', to:'articles#destroy'
 
-  resources :articles
+  resources :articles do 
   resources :comments
-
+  end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
