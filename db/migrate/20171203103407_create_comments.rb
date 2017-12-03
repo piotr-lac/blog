901caedb0e503940446e09fef0db8288e7081f19
Piotr Lac <piotr.lac@protonmail.ch>
2017-12-03 11:57:05 +0100

    right version add comments migration, entry in model of articel about relations

--- db/migrate/20171203103407_create_comments.rb
+++ db/migrate/20171203103407_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
       t.string :commenter
-      t.txt :body
+      t.text :body
       t.references :article, foreign_key: true
 
       t.timestamps
